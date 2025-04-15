45d28a1d441646506d4fe66259e162ed5c2ce39a
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-04-15 11:59:54 +0100

    player methods

--- src/main/java/com/pl/premier_zone/player/PlayerService.java
+++ src/main/java/com/pl/premier_zone/player/PlayerService.java
@@ -1,9 +1,31 @@
 package com.pl.premier_zone.player;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PlayerService {
-  
+    private final PlayerRepository playerRepository;
+    
+    @Autowired
+    public PlayerService(PlayerRepository playerRepository) {
+        this.playerRepository = playerRepository;
+    }
+    
+    public List<Player> getPlayers() {
+        return playerRepository.findAll();
+    }
+    public List<Player> getPlayersFromTeam(String teamName) {
+        return playerRepository.findAll().stream()
+            .filter(player -> teamName.equals(player.getTeam()))
+            .collect(Collectors.toList());
+    }
+    public List<Player> getPlayersByName(String searchText){
+      return playerRepository.findAll().stream()
+        .filter(player -> player.getName().toLowerCase().contains(searchText.toLowerCase()))
+        .collect(Collectors.toList());
+    }
 }
-
