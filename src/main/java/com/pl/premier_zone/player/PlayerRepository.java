a5e98e7a4fe4e785e33f496411c626db5bac4fc6
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-04-20 12:46:24 +0100

    update player repo for new changes

--- src/main/java/com/pl/premier_zone/player/PlayerRepository.java
+++ src/main/java/com/pl/premier_zone/player/PlayerRepository.java
@@ -1,8 +1,10 @@
 package com.pl.premier_zone.player;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -10,4 +12,16 @@ public interface PlayerRepository extends JpaRepository<Player, String> {
     void deleteByName(String playerName);
 
     Optional<Player> findByName(String name);
+    
+    // Add statistics-related queries
+    @Query("SELECT p FROM Player p JOIN p.statistics s ORDER BY s.goals DESC")
+    List<Player> findAllOrderByGoalsDesc();
+    
+    @Query("SELECT p FROM Player p JOIN p.statistics s ORDER BY s.assists DESC")
+    List<Player> findAllOrderByAssistsDesc();
+    
+    // Add comparison-related queries
+    List<Player> findByPosOrderByXgDesc(String position);
+    
+    List<Player> findByTeamOrderByMpDesc(String team);
 }
