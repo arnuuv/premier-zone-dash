e0112d5c9e23fb2d7679bddb3c7c31f1ce701c38
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-04-27 12:01:50 +0100

    fantasy team repo

---
+++ src/main/java/com/pl/premier_zone/fantasy/FantasyTeam.java
@@ -0,0 +1,40 @@
+package com.pl.premier_zone.fantasy;
+
+import com.pl.premier_zone.player.Player;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FantasyTeam {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String name;
+    private String ownerUsername;
+    private String formation;
+    private double budget;
+    private double spentBudget;
+    private int totalPoints;
+    
+    @ManyToMany
+    @JoinTable(
+        name = "fantasy_team_players",
+        joinColumns = @JoinColumn(name = "team_id"),
+        inverseJoinColumns = @JoinColumn(name = "player_id")
+    )
+    private List<Player> players = new ArrayList<>();
+    
+    @ElementCollection
+    @CollectionTable(name = "fantasy_team_captain", joinColumns = @JoinColumn(name = "team_id"))
+    private List<String> captainIds = new ArrayList<>();
+} 
